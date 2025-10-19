cf4fe01e856b7b17aa4516a9fa2f838bae9c8347
Md Selim <mdselim.ibn@gmail.com>
2025-10-19 10:45:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,15 +2,15 @@
 const API_BASE_URL = 'https://perspicacity.onrender.com';
 
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js')
-      .then(registration => {
-        console.log('ServiceWorker registration successful with scope: ', registration.scope);
-      })
-      .catch(err => {
-        console.log('ServiceWorker registration failed: ', err);
-      });
-  });
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/sw.js', { scope: '/' })
+            .then(registration => {
+                console.log('ServiceWorker registration successful with scope: ', registration.scope);
+            })
+            .catch(err => {
+                console.log('ServiceWorker registration failed: ', err);
+            });
+    });
 }
 
 document.addEventListener('DOMContentLoaded', () => {
